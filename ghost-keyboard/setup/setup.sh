3ef594b4c939d0b1f6cb241d545b0218f9fae5b5
Federico Abdo <fabdo@scanntech.com>
2025-03-30 20:36:10 -0300

    Chequea donde esta el archivo config.txt

--- ghost-keyboard/setup/setup.sh
+++ ghost-keyboard/setup/setup.sh
@@ -7,12 +7,21 @@ set -e
 # Treat undefined environment variables as errors.
 set -u
 
+MODULES_PATH='/etc/modules'
+CONFIG_FILE=/boot/firmware/config.txt
+CMDLINE_FILE=/boot/firmware/cmdline.txt
+
+# check if $CONFIG_FILE exists or go back to old path
+if [[ ! -e $CONFIG_FILE ]]; then
+    CONFIG_FILE=/boot/config.txt
+    CMDLINE_FILE=/boot/cmdline.txt
+fi
+
 # Enable the dwc2 kernel driver, which we need to emulate USB devices with USB OTG.
-readonly MODULES_PATH='/etc/modules'
 if ! grep --quiet '^dwc2$' "${MODULES_PATH}" ; then
   echo 'dwc2' | tee --append "${MODULES_PATH}"
 fi
-readonly BOOT_CONFIG_PATH='/boot/config.txt'
+
 if ! grep --quiet '^dtoverlay=dwc2$' "${BOOT_CONFIG_PATH}" ; then
   echo 'dtoverlay=dwc2' | tee --append "${BOOT_CONFIG_PATH}"
 fi
