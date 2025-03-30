b2432b8df8404d80d6455bff46f82f9eee4f26a0
Federico Abdo <fabdo@scanntech.com>
2025-03-30 18:06:30 -0300

    Fix: agrego las variables que faltaban

--- ghost-keyboard/setup/utils/usb-gadget.sh
+++ ghost-keyboard/setup/utils/usb-gadget.sh
@@ -16,6 +16,10 @@ export USB_CONFIG_INDEX=1
 readonly USB_CONFIG_INDEX
 export USB_CONFIG_DIR="configs/c.${USB_CONFIG_INDEX}"
 readonly USB_CONFIG_DIR
+export USB_ALL_CONFIGS_DIR="configs/*"
+readonly USB_ALL_CONFIGS_DIR
+export USB_ALL_FUNCTIONS_DIR="functions/*"
+readonly USB_ALL_FUNCTIONS_DIR
 
 function usb_gadget_activate {
 	ls /sys/class/udc >"${USB_DEVICE_PATH}/UDC"
