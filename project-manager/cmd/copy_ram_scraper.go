42914ee6ce484aedd7fce81892ac7fa55dcb81bc
Federico Abdo <fabdo@scanntech.com>
2025-03-31 14:32:28 -0300

    FIX: ruta de los scripts

--- project-manager/cmd/copy_ram_scraper.go
+++ project-manager/cmd/copy_ram_scraper.go
@@ -8,7 +8,7 @@ import (
 func CopyRamScraper() {
 	fmt.Println("Copiando ram-scraper...")
 
-	cmd := exec.Command("/opt/ram-freezer/bin/ghost-keyboard", "-f", "scripts/windows_copy_ram_scraper")
+	cmd := exec.Command("/opt/ram-freezer/bin/ghost-keyboard", "-f", "/opt/ram-freezer/bin/scripts/windows_copy_ram_scraper")
 
 	output, err := cmd.CombinedOutput()
 	if err != nil {
