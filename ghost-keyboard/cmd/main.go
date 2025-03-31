231cd1b619947a40852a4ed6739412956daaeee3
Federico Abdo <fabdo@scanntech.com>
2025-03-31 14:29:31 -0300

    Agrego la logica de 'wait x' en los scripts para esperar x segundos. Tambien agrego un 'wait 2' luego de abrir el powershell

--- ghost-keyboard/cmd/main.go
+++ ghost-keyboard/cmd/main.go
@@ -7,8 +7,15 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"strconv"
 	"time"
 )
+
+// wait_time is the time to wait between key presses
+var wait_time = 100 * time.Millisecond
+
+// wait_time_key is the time to wait pressing a key
+var wait_time_key = 30 * time.Millisecond
 
 func readFile(path string) (*os.File, error) {
 	file, err := os.Open(path)
@@ -36,10 +43,32 @@ func processFile(scanner bufio.Scanner) error {
 
 	for scanner.Scan() {
 		line := scanner.Text()
+		fmt.Println("Processing line:", line)
+		
+		// wait for a specific time
+		if strings.HasPrefix(line, "wait") {
+			parts := strings.SplitN(line, " ", 2) // Divide en ["wait", "5"]
+			
+			if len(parts) < 2 {
+				fmt.Println("Falta el tiempo después de 'wait': Default 1")
+				parts = append(parts, "1") // Si no hay tiempo, usar 1 segundo por defecto
+			}
+
+			waitTime, err := strconv.Atoi(parts[1]) // Convierte el tiempo a entero
+			if err != nil {
+				fmt.Println("Error: tiempo inválido después de 'wait'")
+				return fmt.Errorf("error: tiempo inválido después de 'wait'")
+			}
+
+			fmt.Printf("Sleeping %d seconds\n", waitTime)
+			time.Sleep(time.Duration(waitTime) * time.Second)
+			continue // Skip to the next line
+		}
+
 		i := 0
 		for i < len(line) {
 			char := line[i]
-
+		
 			if char == '{' {
 				endIdx := strings.Index(line[i:], "}")
 				if endIdx == -1 {
@@ -85,43 +114,50 @@ func writeChar(char uint8, hid *os.File) error {
 		return fmt.Errorf("error writing keypress %d: %w", char, err)
 	}
 
-	time.Sleep(10 * time.Millisecond)
+	time.Sleep(wait_time_key)
 
 	_, err = hid.Write(keycodes.Empty)
 	if err != nil {
 		return fmt.Errorf("error unpressing key: %w", err)
 	}
 
+	fmt.Printf("Sleeping %v\n", wait_time)
+	time.Sleep(wait_time) // Sleep for a while to simulate key press duration
+	fmt.Printf("Wrote key: %c, 0x%X\n", char, keycode)
+
 	return nil
 }
 
+
 func writeSpecialKey(key string, hid *os.File) {
 	var modKey byte = keycodes.KEYCODE_NONE
+
 	keys := make([]byte, 0)
 
 	if strings.Contains(key, "+") {
-		parts := strings.Split(key, " + ")
+			parts := strings.Split(key, " + ")
 
-		for _, part := range parts {
-			if k, ok := keycodes.ModifierKey[part]; ok {
-				modKey |= k
-			} else if k, ok := keycodes.SpecialKey[part]; ok {
-				keys = append(keys, k)
-			} else if len(part) == 1 {
-				r := rune(part[0])
-				if k, ok := keycodes.Key[r]; ok {
+			for _, part := range parts {
+				if k, ok := keycodes.ModifierKey[part]; ok {
+					modKey |= k
+				} else if k, ok := keycodes.SpecialKey[part]; ok {
 					keys = append(keys, k)
-				} else if k, ok := keycodes.KeyShift[r]; ok {
-					modKey |= keycodes.KEYCODE_LEFT_SHIFT
-					keys = append(keys, k)
+				} else if len(part) == 1 {
+					r := rune(part[0])
+					if k, ok := keycodes.Key[r]; ok {
+						keys = append(keys, k)
+					} else if k, ok := keycodes.KeyShift[r]; ok {
+						modKey |= keycodes.KEYCODE_LEFT_SHIFT
+						keys = append(keys, k)
+					}
+				}
+
+				if len(keys) >= 6 {
+					break
 				}
 			}
-
-			if len(keys) >= 6 {
-				break
-			}
-		}
 	} else {
+		// Si no hay +, solo se procesa la tecla
 		if k, ok := keycodes.ModifierKey[key]; ok {
 			modKey |= k
 		} else if k, ok := keycodes.SpecialKey[key]; ok {
@@ -140,19 +176,23 @@ func writeSpecialKey(key string, hid *os.File) {
 	for len(keys) < 6 {
 		keys = append(keys, keycodes.KEYCODE_NONE)
 	}
-
+	
 	_, err := hid.Write([]byte{modKey, 0x00, keys[0], keys[1], keys[2], keys[3], keys[4], keys[5]})
 	if err != nil {
 		fmt.Printf("Error writing keypress: %v\n", err)
 		return
 	}
-	time.Sleep(10 * time.Millisecond)
+	time.Sleep(wait_time_key)
 
 	_, err = hid.Write(keycodes.Empty)
 	if err != nil {
 		fmt.Printf("Error unpressing key: %v\n", err)
 		return
 	}
+
+	fmt.Printf("Sleeping %v\n", wait_time)
+	time.Sleep(wait_time)
+	fmt.Printf("Wrote key: %s, %x, %v\n", key, modKey, keys)
 }
 
 func main() {
