843e0d671931d2d68fddad572b40ce1ea7c17b57
DuckyCB <duckycb@proton.me>
2025-04-02 19:25:43 -0300

    use internal storage if external is not connected

--- vault/setup/init-storage.sh
+++ vault/setup/init-storage.sh
@@ -5,25 +5,33 @@ set -u
 
 source "/opt/ram-freezer/utils/usb-setup/usb-gadget.sh"
 
-# TODO: se podria usar un almacenamiento emulado si /dev/sda1 no existe
-#dd if=/dev/zero of=/piusb.bin bs=1M count=1024
-#mkdosfs /piusb.bin -F 32 -I
-USB_PATH="/dev/sda1"
-
 mkdir -p "${USB_STORAGE_FUNCTIONS_DIR}"
 echo 1 > "${USB_STORAGE_FUNCTIONS_DIR}/stall"
 echo 0 > "${USB_STORAGE_FUNCTIONS_DIR}/lun.0/removable"
-echo "${USB_PATH}" > "${USB_STORAGE_FUNCTIONS_DIR}/lun.0/file"
+## Use USB drive and create internal storage as fallback
+if [ -e "${USB_DRIVE_PATH}" ]; then
+    echo "${USB_DRIVE_PATH}" > "${USB_STORAGE_FUNCTIONS_DIR}/lun.0/file"
+else
+    echo "Almacenamiento USB no conectado, utilizando almacenamiento interno"
+
+    if [ ! -e "${LOCAL_STORAGE_FILE}" ]; then
+        dd if=/dev/zero of="${LOCAL_STORAGE_FILE}" bs=1M count="${LOCAL_STORAGE_SIZE}" status=progress
+        mkdosfs "${LOCAL_STORAGE_FILE}" -F 32 -I
+    else
+        echo "Usando archivo de almacenamiento existente: ${LOCAL_STORAGE_FILE}"
+    fi
+
+    echo "${LOCAL_STORAGE_FILE}" > "${USB_STORAGE_FUNCTIONS_DIR}/lun.0/file"
+fi
 #echo 0 > "${USB_STORAGE_FUNCTIONS_DIR}/lun.0/cdrom"
 #echo 0 > "${USB_STORAGE_FUNCTIONS_DIR}/lun.0/ro"
 #echo 0 > "${USB_STORAGE_FUNCTIONS_DIR}/lun.0/nofua"
-#echo /piusb.bin > "${USB_STORAGE_FUNCTIONS_DIR}/lun.0/file"
 
 
 ln -s "${USB_STORAGE_FUNCTIONS_DIR}" "${USB_CONFIG_DIR}/"
 
 # Storage device label
 # Para FAT32 o exFATg
-dosfslabel "${USB_PATH}" "${USB_STORAGE_DEVICE_NAME}"
+dosfslabel "${USB_DRIVE_PATH}" "${USB_STORAGE_DEVICE_NAME}"
 
 printf "Vault configurado\n"
