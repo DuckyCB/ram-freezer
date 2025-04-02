29af1a94a6aa9d04e4af21d7f975f13446628b9d
Federico Abdo <fabdo@scanntech.com>
2025-04-02 14:41:25 -0300

    fix

--- project-manager/cmd/copy_ram_scraper_to_usb.go
+++ project-manager/cmd/copy_ram_scraper_to_usb.go
@@ -9,13 +9,28 @@ func CopyRamScraperToUSB() {
 	fmt.Println("Copiando ram-scraper al USB...")
 
 	// Montar el USB
+	fmt.Println("Montando el USB...")
 	cmd := exec.Command("mount", "/dev/sda1", "/mnt/usb/")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Error montando el USB:", err)
+		return
+	}
+	fmt.Println(string(output))
 
 	// Crear el directorio en el USB
-	cmd := exec.Command("mkdir", "-p", "/mnt/usb/ram-scraper/")
+	fmt.Println("Creando el directorio en el USB...")
+	cmd = exec.Command("mkdir", "-p", "/mnt/usb/ram-scraper/")
+	output, err = cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Error creando el directorio:", err)
+		return
+	}
 
-	cmd := exec.Command("cp", "-r", "/opt/ram-freezer/", "/mnt/usb/ram-scraper/")
-	output, err := cmd.CombinedOutput()
+	// Copiar los archivos de ram-scraper al USB
+	fmt.Println("Copiando los archivos de ram-scraper al USB...")
+	cmd = exec.Command("cp", "-r", "/opt/ram-freezer/", "/mnt/usb/ram-scraper/")
+	output, err = cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println("Error copiando ram-scraper:", err)
 		return
