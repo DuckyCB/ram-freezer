5d47b820aea531e4ca68437a2dc9cc04beeb4596
Federico Abdo <fabdo@scanntech.com>
2025-04-02 22:25:58 -0300

    permisos

--- remove.sh
+++ remove.sh
@@ -2,6 +2,9 @@
 
 printf "ELIMINANDO INSTALACIÓN\n\n\n"
 
+chmod -x /opt/ram-freezer/ghost-keyboard/setup/remove.sh
 /opt/ram-freezer/ghost-keyboard/setup/remove.sh
 
+
+chmod -x /opt/ram-freezer/project-manager/setup/remove.sh
 /opt/ram-freezer/project-manager/setup/remove.sh
