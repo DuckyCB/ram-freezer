30787953d9555926a03a9952726af7ba9d1f932c
Federico Abdo <fabdo@scanntech.com>
2025-04-02 22:15:22 -0300

    Copio el modulo ram_scraper al dispositivo USB

--- project-manager/cmd/copy_ram_scraper_to_usb.go
+++ project-manager/cmd/copy_ram_scraper_to_usb.go
@@ -3,35 +3,26 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"time"
 )
 
 func CopyRamScraperToUSB() {
 	fmt.Println("Copiando ram-scraper al USB...")
 
-	// Montar el USB si no esta montado
-	cmd := exec.Command("bash", "-c", "'mount | grep /dev/sda1'")
-	output, err := cmd.CombinedOutput()
-	fmt.Println(string(output))
-
-	// si err es 1 significa que el USB no esta montado
-	if err != nil && string(output) != "" {
-		fmt.Println("Error al listar los dispositivos montados:", err)
-		return
-	}
-	
-
 	// Verificar si el USB está montado
-	if string(output) == "" {
-		fmt.Println("El USB no está montado. Montando el USB...")
+	cmd := exec.Command("bash", "-c", "mount | grep '/dev/sda1'")
+	output, err := cmd.CombinedOutput()
 
-		fmt.Println("Montando el USB...")
-		cmd := exec.Command("sudo", "mount", "/dev/sda1", "/mnt/usb/")
-		output, err := cmd.CombinedOutput()
+	if err != nil || len(output) == 0 {
+		fmt.Println("El USB no está montado. Intentando montarlo...")
+
+		cmd = exec.Command("sudo", "mount", "/dev/sda1", "/mnt/usb/")
+		output, err = cmd.CombinedOutput()
 		if err != nil {
-			fmt.Println("Error montando el USB:", err)
+			fmt.Println("Error montando el USB:", string(output))
 			return
 		}
-		fmt.Println(string(output))
+		fmt.Println("USB montado correctamente.")
 	} else {
 		fmt.Println("El USB ya está montado.")
 	}
@@ -41,57 +32,52 @@ func CopyRamScraperToUSB() {
 	cmd = exec.Command("sudo", "mkdir", "-p", "/mnt/usb/ram-scraper/")
 	output, err = cmd.CombinedOutput()
 	if err != nil {
-		fmt.Println("Error creando el directorio:", err)
+		fmt.Println("Error creando el directorio:", string(output))
 		return
 	}
 
 	// Copiar los archivos de ram-scraper al USB
 	fmt.Println("Copiando los archivos de ram-scraper al USB...")
-	cmd = exec.Command("sudo", "cp", "-r", "/opt/ram-freezer/", "/mnt/usb/ram-scraper/")
+	cmd = exec.Command("sudo", "cp", "-r", "/opt/ram-freezer/ram-scraper/.", "/mnt/usb/ram-scraper/")
 	output, err = cmd.CombinedOutput()
 	if err != nil {
-		fmt.Println("Error copiando ram-scraper:", err)
+		fmt.Println("Error copiando ram-scraper:", string(output))
 		return
 	}
-	fmt.Println(string(output))
-
+	fmt.Println("Archivos copiados correctamente.")
 
 	// Desmontar el USB
 	fmt.Println("Desmontando el USB...")
 	cmd = exec.Command("sudo", "umount", "/mnt/usb/")
-
 	output, err = cmd.CombinedOutput()
 	if err != nil {
-		fmt.Println("Error desmontando el USB:", err)
+		fmt.Println("Error desmontando el USB:", string(output))
 		return
 	}
-	fmt.Println(string(output))
+	fmt.Println("USB desmontado correctamente.")
 
-	// Reconectando el USB
+	// Reconectar el USB
 	fmt.Println("Reconectando el USB...")
-
-	cmd = exec.Command("echo", "", "|", "sudo", "tee", "/sys/kernel/config/usb_gadget/ram-freezer/UDC")
+	cmd = exec.Command("bash", "-c", "echo '' | sudo tee /sys/kernel/config/usb_gadget/ram-freezer/UDC")
 	output, err = cmd.CombinedOutput()
 	if err != nil {
-		fmt.Println("Error reconectando el USB:", err)
+		fmt.Println("Error reconectando el USB:", string(output))
 		return
 	}
-	fmt.Println(string(output))
 
-	// wait 1s
-	cmd = exec.Command("sleep", "1")
+	// sleep 1 seconds
+	fmt.Println("Esperando 3 segundos...")
+	time.Sleep(3 * time.Second)
 
-	// ls /sys/class/udc
-	cmd = exec.Command("ls", "/sys/class/udc", "|", "sudo", "tee", "/sys/kernel/config/usb_gadget/ram-freezer/UDC")
+	cmd = exec.Command("bash", "-c", "ls /sys/class/udc | sudo tee /sys/kernel/config/usb_gadget/ram-freezer/UDC")
 	output, err = cmd.CombinedOutput()
 	if err != nil {
-		fmt.Println("Error listando el USB:", err)
+		fmt.Println("Error listando el USB:", string(output))
 		return
 	}
-	fmt.Println(string(output))
 
-	// sleep 5
-	cmd = exec.Command("sleep", "5")
+	fmt.Println("USB reconectado correctamente. Esperando 8 segundos")
+	time.Sleep(8 * time.Second)
 
 	fmt.Println("Proceso de copia de ram-scraper al USB completado.")
 }
